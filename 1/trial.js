7c51d12d09af78b57456884c810365321646323c
Sahil Dahat <sahil.developerinfo@gmail.com>
2021-10-13 00:59:17 +0530

    FreeCodeCamp - JS

---
+++ 1/trial.js
@@ -0,0 +1,66 @@
+//document.getElementById("count-el").innerText=5
+
+/*
+let count = 0
+console.log(count)
+
+let myage = 100 - 31
+console.log(myage)
+*/
+
+/*
+let batch1 = 50
+let batch2 = 25
+
+let count = batch1 + batch2
+console.log(count)
+*/
+
+/*
+let myage = 10
+let humandogratio = 7
+
+let amount = myage*humandogratio
+
+console.log(amount)
+*/
+
+/*
+Because JS works from up to down side, the latest value of 'count'
+variable will be taken in consideration.
+*/
+
+/*
+let count =5
+
+count =3
+
+console.log(count)
+*/
+
+/*
+let count = 5
+
+count = count +1
+
+console.log(count)
+*/
+
+/*
+let BonusPoints = 50
+console.log(BonusPoints)
+
+BonusPoints = BonusPoints +100
+console.log(BonusPoints)
+
+BonusPoints = BonusPoints -25
+console.log(BonusPoints)
+
+BonusPoints = BonusPoints +70
+console.log(BonusPoints)
+*/
+
+
+function increment() {
+    console.log("The button was clicked")
+}
---
+++ 2/index.js
@@ -0,0 +1,41 @@
+/*
+function increment(){
+    console.log("clicked")
+}
+*/
+
+/*
+function countdown(){
+    console.log(5)
+    console.log(4)
+    console.log(3)
+    console.log(2)
+    console.log(1)
+}
+countdown()
+
+countdown()
+*/
+/*
+function noname(){
+    console.log(42)
+}
+
+noname()
+*/
+
+/*
+let lap1 = 30
+let lap2 = 60
+let lap3 = 20
+
+function laptime(){
+    let totaltime = lap1 + lap2 + lap3
+    console.log(totaltime)
+}
+// let variable(block scoped) is defined inside this block of code.
+
+laptime()
+*/
+
+
